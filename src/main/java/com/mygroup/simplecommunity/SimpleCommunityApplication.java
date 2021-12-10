3eb83a3a602183dd110f4e6c3003baa213a81661
kom-1010 <kom-1010@naver.com>
2021-12-10 11:09:18 +0900

    Initial commit

---
+++ src/main/java/com/mygroup/simplecommunity/SimpleCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.mygroup.simplecommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCommunityApplication.class, args);
+	}
+
+}
